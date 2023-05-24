fdcf7426e6ea049a3f9f790d4caeffc166c364a7
beksultanbatyrbekow <beksultanb098@gmail.com>
2023-05-24 16:26:12 +0600

    hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Square = ({ color, onClick }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100px',
+        height: '100px',
+        backgroundColor: color,
+        margin: '10px',
+      }}
+      onClick={onClick}
+    />
+  );
+};
+
+const App = () => {
+  const [color1, setColor1] = useState('blue');
+  const [color2, setColor2] = useState('red');
+
+  const handleClick = () => {
+    setColor1(color1 === 'red' ? 'blue' : 'red');
+    setColor2(color2 === 'red' ? 'blue' : 'red');
+  };
+
+  return (
+    <div>
+      <Square color={color1} onClick={handleClick} />
+      <Square color={color2} onClick={handleClick} />
     </div>
   );
-}
+};
 
 export default App;
